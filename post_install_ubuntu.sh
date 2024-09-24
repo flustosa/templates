9b11e0ac2c2e35a73c710a42164806ce1c446976
Felipe Lustosa <68484346+flustosa@users.noreply.github.com>
2024-09-24 10:51:11 -0300

    Update post_install_ubuntu.sh

--- post_install_ubuntu.sh
+++ post_install_ubuntu.sh
@@ -281,20 +281,20 @@ setup_vim () {
 
 
 ################# AUTO UPDATE  + CRONTAB #######################
+CRON_PATH="/usr/local/sbin"
 
 create_cron () {
-	CRON_PATH="/usr/local/sbin/"
-	CRON="30 0 * * * /usr/local/sbin/autoupdate.sh"
-	cp ./tempcron ./tempcron.copy 2>/dev/null
-	rm ./tempcron
-	(sudo crontab -l 2>/dev/null 1>./tempcron) && (printf "\n" >> ./tempcron && printf "%s" "$CRON" >> ./tempcron && printf "\n" >>./tempcron)
-	sudo crontab ./tempcron
+	CRON="30 0 * * * $CRON_PATH/autoupdate.sh"
+	cp $CRON_PATH/tempcron $CRON_PATH/tempcron.copy 2>/dev/null
+	rm $CRON_PATH/tempcron
+	(sudo crontab -l 2>/dev/null 1>$CRON_PATH/tempcron) && (printf "\n" >> $CRON_PATH/tempcron && printf "%s" "$CRON" >> $CRON_PATH/tempcron && printf "\n" >>$CRON_PATH/tempcron)
+	sudo crontab $CRON_PATH/tempcron
 }
 
 
 create_autoupdate () {
-	curl -o ./autoupdate.sh https://raw.githubusercontent.com/flustosa/templates/master/autoupdate.sh
-	chmod +x ./autoupdate.sh
+	curl -o $CRON_PATH/autoupdate.sh https://raw.githubusercontent.com/flustosa/templates/master/autoupdate.sh
+	chmod +x $CRON_PATH/autoupdate.sh
 	create_cron	
 	echo -e "$ACTION - Deseja criar um alerta no healthcheck.io? [Y/n]"
 	if confirm; then
@@ -306,10 +306,21 @@ create_autoupdate () {
 setup_healthcheck_io () {
 	echo -e "$ACTION - Informe a chave (PING_KEY) para configuração dos alertas no healthcheck.io:"
 	read PING_KEY
-	printf "PING_KEY=$PING_KEY" > /usr/local/sbin/.env
-	printf "SLUG=$(hostname)" >> /usr/local/sbin/.env
+	
+	printf "PING_KEY=$PING_KEY" > $CRON_PATH/.env
+	printf "\n" >> $CRON_PATH/.env
+	
+	# Use system's hostname as check's slug
+	SLUG=$(hostname)
+	printf $SLUG >> $CRON_PATH/.env
+	
+	# Construct a ping URL and append "?create=1" at the end:
+	URL=https://hc-ping.com/$PING_KEY/$SLUG?create=1
 
+	# Send success signal to Healthchecks.io
+	curl -m 10 --retry 5 $URL
 }
+
 
 
 ################## ATUALIZACAO FINAL ##################
