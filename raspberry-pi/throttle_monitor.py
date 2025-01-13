f564e3093ec06a214f7f1609c8e56ca940747922
flustosa <felipe.lustosa@gmail.com>
2025-01-13 14:38:51 -0300

    Added gatus push monitoring

--- raspberry-pi/throttle_monitor.py
+++ raspberry-pi/throttle_monitor.py
@@ -1,29 +1,53 @@
+import datetime
+import http.client
+import os
 import subprocess
 import time
-import datetime
+
 
 def get_throttled_state():
     result = subprocess.run(['vcgencmd', 'get_throttled'], stdout=subprocess.PIPE)
     state_str = result.stdout.decode().strip()
     return state_str
 
-def log_throttled_state(state, logfile):
-    with open(logfile, 'a') as f:
-        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        f.write(f"{current_time} - Throttled State: {state}\n")
+def log_throttled_state(state):
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    log_message = f"{current_time} - Throttled State: {state}"
+    subprocess.run(['logger', log_message])
+
+def gatus_push(status):
+    host = "10.11.12.163"
+    port = "8080"
+    url = f"/api/v1/endpoints/core_pi-throttle/external?success={status}"
+    token = f'Bearer {os.getenv("GATUS_TOKEN")}'
+    conn = http.client.HTTPConnection(host, port, timeout=10)
+    header = {"Host": host, "Authorization": token}
+    conn.request("POST", url, headers=header)
+    response = conn.getresponse()
+
 
 def main():
-    logfile = "/var/log/throttle_monitor.log"
+
     previous_state = None
     
     while True:
         current_state = get_throttled_state()
-        
-        if current_state != previous_state:
-            log_throttled_state(current_state, logfile)
+        print(f'current_state: {current_state}')
+        log_throttled_state(current_state)
+        if current_state != 'throttled=0x0':
+            log_throttled_state(current_state)
+            status = 'false'
             previous_state = current_state
+        else:
+            status = 'true'
+        try:
+            gatus_push(status)
+        except Exception as e:
+            log_throttled_state(e)
+
         
-        time.sleep(60)  # Check every 60 seconds
+        time.sleep(300)  # Check every 300 seconds
+
 
 if __name__ == "__main__":
     main()
